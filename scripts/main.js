2a48acd15bb5a7c046678d509c7781632ac7accf
FreezeMandu <sewolminsuk03@gmail.com>
2022-11-12 21:03:12 +0900

    모드블럭 추가

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
 Blocks.shieldProjector.targetable = false
 Blocks.shieldProjector.radius = 40
+Blocks.shieldProjector.health = 2000
 Blocks.powerSource.targetable = false
+Blocks.powerSource.health = 200000
