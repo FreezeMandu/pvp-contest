380e0278e73c9f9138c9bac1f646a52b7a16ccc1
FreezeMandu <sewolminsuk03@gmail.com>
2022-11-19 00:24:24 +0900

    응애

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 Blocks.shieldProjector.targetable = false
 Blocks.shieldProjector.radius = 40
 Blocks.shieldProjector.health = 2000
+
+//Blocks.largeShieldProjector.radius = 200
+//Blocks.largeShieldProjector.targetable = false
+//Blocks.largeShieldProjector.health = 2000
+
 Blocks.powerSource.targetable = false
-Blocks.powerSource.health = 200000
+Blocks.powerSource.health = 2000
+
+Blocks.coreShard.targetable = false
+Blocks.coreShard.health = 8000
